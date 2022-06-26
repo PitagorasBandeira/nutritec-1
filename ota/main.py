9ca1d3f98ba7d804cf01ee57c868785e85eec94f
PitagorasBandeira <42848167+PitagorasBandeira@users.noreply.github.com>
2022-06-26 09:54:08 -0300

    ++++++++++v2

--- ota/main.py
+++ ota/main.py
@@ -34,7 +34,7 @@ def reset_mac():
 def pub_sub():
     try:
         while True:
-            print("Listening ++++++++++v1")
+            print("Listening ++++++++++v2")
             mqtt_client.reconnect()
             subscribe_topic = get_c2d_topic(survey_data['device_id'])
             mqtt_client.set_callback(callback_handler)
