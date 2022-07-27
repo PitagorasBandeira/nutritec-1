b6e2fabc2fd8b9ccab21cb7109e33460d4558e3e
Pitágoras Bandeira <pithagoras@hotmail.com>
2022-07-27 16:55:24 -0300

    Update main.py

--- ota/main.py
+++ ota/main.py
@@ -1,14 +1,10 @@
 
-from util import create_mqtt_client, get_telemetry_topic, get_c2d_topic, parse_connection, open_json, web_register_uix, sensor_get_values, reles, bomba1, bomba2
-import utime
-import _thread
-from util import fitaled1
+from util import create_mqtt_client, get_telemetry_topic, get_c2d_topic, parse_connection, open_json, sensor_get_values, reles_on, reles_off, bomba1_on,bomba1_off, bomba2_on, bomba2_off, fitaled_on, fitaled_off
+from time import sleep
 import json
 import gc
-import time
 import senko
-
-import machine
+from machine import reset
 
 gc.collect()
 gc.enable()
@@ -28,13 +24,15 @@ def callback_handler(topic, message_receive):
  
 def reset_mac():
   print("Reiniciando a pedido")
-  machine.reset()
+  reset()
+  
 
 #collect from topic
 def pub_sub():
+    global datadataset_dec_rep_j
     try:
         while True:
-            print("Listening v1")
+            print("Listening - ", str(localtime()))
             mqtt_client.reconnect()
             subscribe_topic = get_c2d_topic(survey_data['device_id'])
             mqtt_client.set_callback(callback_handler)
@@ -45,37 +43,69 @@ def pub_sub():
                 dataset_dec = dataset.decode("utf-8")
                 dataset_dec_rep = dataset_dec.replace("'","\"")
                 datadataset_dec_rep_j = set = json.loads(dataset_dec_rep)
-                if datadataset_dec_rep_j['act'] == "bomba1": bomba1()
-                elif datadataset_dec_rep_j['act'] == "bomba2": bomba2()
-                elif datadataset_dec_rep_j['act'] == "reset": reset_mac()
-                elif datadataset_dec_rep_j['act'] == "led1": fitaled1.value(not fitaled1.value())
-                elif datadataset_dec_rep_j['act'] == "rele": reles()
-                elif datadataset_dec_rep_j['act'] == "keepa": print("keepa")
-                elif datadataset_dec_rep_j['act'] == "getdata": 
-                    data = sensor_get_values()
-                    topic = get_telemetry_topic(survey_data['device_id'])
-                    mqtt_client.publish(topic=topic, msg=data)
-                else: print("")
+                try:
+                    if datadataset_dec_rep_j['act'] == 'bomba1_off': 
+                        bomba1_off()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                    elif datadataset_dec_rep_j['act'] == 'bomba1_on': 
+                        bomba1_on()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                        sleep(30)
+                        bomba1_off()
+                    elif datadataset_dec_rep_j['act'] == 'bomba2_off': 
+                        bomba2_off()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                    elif datadataset_dec_rep_j['act'] == 'bomba2_on': 
+                        bomba2_on()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                        sleep(30)
+                        bomba2_off()
+                    elif datadataset_dec_rep_j['act'] == "reset": reset_mac()
+                    elif datadataset_dec_rep_j['act'] == 'fitaled_off': 
+                        fitaled_off()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data) 
+                    elif datadataset_dec_rep_j['act'] == 'fitaled_on': 
+                        fitaled_on() 
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)                     
+                    elif datadataset_dec_rep_j['act'] == 'reles_off': 
+                        reles_off()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                    elif datadataset_dec_rep_j['act'] == 'reles_on': 
+                        reles_on()
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)                   
+                    elif datadataset_dec_rep_j['act'] == "keepa": print("keepa")
+                    elif datadataset_dec_rep_j['act'] == "getdata": 
+                        data = sensor_get_values()
+                        topic = get_telemetry_topic(survey_data['device_id'])
+                        mqtt_client.publish(topic=topic, msg=data)
+                    else: print("")
+                except: 
+                    print("erro - payload enviado: ",datadataset_dec_rep_j)
             else:
                 mqtt_client.check_msg()
-                utime.sleep(1)
+                sleep(1)
             mqtt_client.disconnect()
-    except Exception as e: print("Sub function error: ", e)
-
-#while True:
-#    sub()
-#    utime.sleep(5)
-_thread.start_new_thread(pub_sub, ())
-#_thread.start_new_thread(res, ())
-#_thread.start_new_thread(sub, ())
-#web_register_uix()
+    except Exception as e: 
+        print("Sub function error: ", e)
+        mqtt_client.disconnect()
 
 
-
-
-
-
-
-
-
+while True:
+  pub_sub()
 
